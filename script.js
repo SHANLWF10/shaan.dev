dd59cd4c2495b1966acb1c8936d547e6fd800ee9
Shaan <shanali.ozhorik@Gmail.com>
2024-07-14 16:44:58 +0500

    .

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 let navbar = document.getElementById("navbar");
 let menuBtn = document.getElementById("span");
 
-menuBtn.addEventListener("click",()=>{
-    navbar.classList.toggle("show")
+menuBtn.addEventListener("click", () => {
+  navbar.classList.toggle("show");
 });
-
 
 // Code under this is for GSAP animations..
